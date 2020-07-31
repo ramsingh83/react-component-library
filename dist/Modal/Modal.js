bd3ab046c03d88af98f4bcef758250b1eea7c621
Michael Clayton <michael.clayton@royalmail.com>
2020-07-31 11:29:34 +0100

    Add dist files

--- dist/Modal/Modal.js
+++ dist/Modal/Modal.js
@@ -15,22 +15,31 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _Icon = _interopRequireDefault(require("../Icon/Icon"));
 
+var _useClickAway = _interopRequireDefault(require("../../hooks/useClickAway"));
+
+require("./modal.scss");
+
 var Modal = function Modal(props) {
   var id = props.id,
       children = props.children,
       dialogTitle = props.dialogTitle,
       dialogDescription = props.dialogDescription,
       closed = props.closed;
-  var closeButton = (0, _react.useRef)(null);
+  var closeButton = (0, _react.useRef)();
+  var rootRef = (0, _react.useRef)();
   (0, _react.useEffect)(function () {
     closeButton.current.focus();
   }, []);
+  (0, _useClickAway.default)(rootRef, function (e) {
+    closed(e);
+  });
   return /*#__PURE__*/_react.default.createElement("div", {
     id: id,
     className: "modal",
     role: "dialog",
     "aria-labelledby": dialogTitle,
-    "aria-describedby": dialogDescription
+    "aria-describedby": dialogDescription,
+    ref: rootRef
   }, /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
     ref: closeButton,
